4664da7a8e30e8f5d4d673f089766b8a1e949ae7
JeffBcyc <41636467+JeffBcyc@users.noreply.github.com>
2020-07-27 00:22:13 -0400

    using Rider to commit changes

---
+++ Assets/Scripts/TextTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+public class TextTrigger : MonoBehaviour
+{
+
+    [SerializeField] private RevealText textToTrigger;
+    [SerializeField] private RevealText textToDisable;
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag("Player")) return;
+        textToTrigger.gameObject.SetActive(true);
+        textToTrigger.StartRollingText();
+        try
+        {
+            textToDisable.gameObject.SetActive(false);
+        }
+        catch
+        {
+            Debug.Log("nothing happens");
+        }
+        Destroy(gameObject);
+
+    }
+
+    
+}
